8efd8fa692f43771fa8ac8e7932dcff44b8d363f
Valentinas <scenass@gmail.com>
2022-03-29 22:50:55 +0300

    Dev

--- server/index.ts
+++ server/index.ts
@@ -2,14 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import { UserModel } from './models/Users';
 
 const app = express();
 
-// app.listen(3001, () => {
-//   console.log('servers');
-// });
-
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: '30mb' }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
@@ -19,7 +16,28 @@ const CONNECTION_URL =
   'mongodb+srv://vinyl:Realrecordshop3@cluster0.dsafo.mongodb.net/vinylDB?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(CONNECTION_URL)
-  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-  .catch((error) => console.log(error.message));
+mongoose.connect(CONNECTION_URL);
+// .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+// .catch((error) => console.log(error.message));
+
+// app.get('/getUsers', (req, res) => {
+//   UserModel.find({}, (err, result) => {
+//     if (err) {
+//       res.json(err);
+//     } else {
+//       res.json(result);
+//     }
+//   });
+// });
+
+// app.listen(PORT, () => {
+//   console.log('Works');
+// });
+
+// app.post('/createUser', async (req, res) => {
+//   const user = req.body;
+//   const newUser = new UserModel(user);
+//   await newUser.save();
+
+//   res.json(user);
+// });
