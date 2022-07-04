6dd6a9d002c0a4ff2ef735994631fbddcdda1039
Valentinas <scenass@gmail.com>
2022-07-04 13:37:12 +0300

    chore: created .env.example files

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,9 +1,19 @@
 import Header from 'common/layouts/Header';
+import { useState } from 'react';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
   return (
     <>
       <Header />
+      <h1></h1>
       <div>Login</div>
     </>
   );
--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,6 +1,15 @@
 import Header from 'common/layouts/Header';
+import { useState } from 'react';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
   return (
     <>
       <Header />
