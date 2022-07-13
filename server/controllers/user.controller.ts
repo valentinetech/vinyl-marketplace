cf4817094fab97d55660f21bc2f0db66b6e2b55a
Valentinas <scenass@gmail.com>
2022-07-13 22:42:42 +0300

    Feat: Auth, user login server

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -50,7 +50,21 @@ export const registerUser = asyncHandler(async (req: Request, res: Response) => 
 // @route    POST /api/users/login
 // @access   Public
 export const loginUser = asyncHandler(async (req: Request, res: Response) => {
-  res.json({ message: 'Register user' });
+  const { email, password } = req.body;
+
+  // Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid credentials');
+  }
 });
 
 // @desc     Get user data
