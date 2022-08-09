aeaec2e8555316062887cc455a11507a99e88ea5
Valentinas <scenass@gmail.com>
2022-08-10 00:11:38 +0300

    FIX: Deploy test

--- client/src/features/Auth/slices/authService.ts
+++ client/src/features/Auth/slices/authService.ts
@@ -13,7 +13,8 @@ export interface LoginProps {
 	password: string;
 }
 
-const API_USERS_URL = API_URL + '/api/users/';
+// const API_USERS_URL = API_URL + '/api/users/';
+const API_USERS_URL = 'https://vinylauction.herokuapp.com/api/users/';
 
 const register = async (userData: RegisterProps) => {
 	const { data } = await axios.post(API_USERS_URL + 'register', userData);
