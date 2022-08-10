1905b29ba827efdc5db825a57851df234da7c7b7
Valentinas <scenass@gmail.com>
2022-08-10 10:49:12 +0300

    REVERT: Reverted changes

--- server/server.ts
+++ server/server.ts
@@ -9,61 +9,75 @@ import userRoutes from './routes/User.routes';
 const path = require('path');
 const router = express();
 
+/** Connect to Mongo */
 mongoose
 	.connect(config.mongo.url, { retryWrites: true, w: 'majority' })
 	.then(() => {
 		Logging.info('Mongo connected successfully.');
+		StartServer();
 	})
 	.catch((error) => Logging.error(error));
 
-router.use((req, res, next) => {
-	Logging.info(`Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+/** Only Start Server if Mongoose Connects */
+const StartServer = () => {
+	/** Log the request */
+	router.use((req, res, next) => {
+		/** Log the req */
+		Logging.info(`Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
 
-	res.on('finish', () => {
-		Logging.info(
-			`Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`
-		);
+		res.on('finish', () => {
+			/** Log the res */
+			Logging.info(
+				`Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`
+			);
+		});
+
+		next();
 	});
 
-	next();
-});
+	router.use(express.urlencoded({ extended: true }));
+	router.use(express.json());
 
-router.use(express.json());
-router.use(express.urlencoded({ extended: false }));
+	/** Rules of our API */
+	router.use((req, res, next) => {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
-// router.use((req, res, next) => {
-// 	res.header('Access-Control-Allow-Origin', '*');
-// 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+		if (req.method == 'OPTIONS') {
+			res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+			return res.status(200).json({});
+		}
 
-// 	if (req.method == 'OPTIONS') {
-// 		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-// 		return res.status(200).json({});
-// 	}
-
-// 	next();
-// });
-
-router.use('/api/auctions', auctionRoutes);
-router.use('/api/users', userRoutes);
-
-router.get('/ping', (req, res, next) => res.status(200).json({ ping: 'pong' }));
-
-if (process.env.NODE_ENV === 'prod') {
-	router.use(express.static(path.join(__dirname, '../client/build')));
-
-	router.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')));
-} else {
-	router.get('/', (req, res) => res.send('Please set to prod'));
-}
-
-router.use((req, res, next) => {
-	const error = new Error('Not found');
-
-	Logging.error(error);
-
-	res.status(404).json({
-		message: error.message,
+		next();
 	});
-});
-const port = process.env.PORT || 5000;
-router.listen(port, () => Logging.info(`Server is running on port ${port}`));
+
+	/** Routes */
+	router.use('/api/auctions', auctionRoutes);
+	router.use('/api/users', userRoutes);
+
+	if (process.env.NODE_ENV === 'prod') {
+		router.use(express.static(path.join(__dirname, '../client/build')));
+
+		router.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')));
+	} else {
+		router.get('/', (req, res) => res.send('Please set to prod'));
+	}
+
+	/** Healthcheck */
+	router.get('/ping', (req, res, next) => res.status(200).json({ ping: 'pong' }));
+
+	/** Error handling */
+	router.use((req, res, next) => {
+		const error = new Error('Not found');
+
+		Logging.error(error);
+
+		res.status(404).json({
+			message: error.message,
+		});
+	});
+
+	http
+		.createServer(router)
+		.listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}`));
+};
