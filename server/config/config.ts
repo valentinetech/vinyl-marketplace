d29362e7ef045c0a38abac4dbd35ef89e8fb8841
Valentinas <scenass@gmail.com>
2022-08-10 11:56:05 +0300

    DEPLOY: Heroku commit

--- server/config/config.ts
+++ server/config/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const MONGO_URL = process.env.MONGO_URL;
 
-const SERVER_PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const SERVER_TOKEN_EXPIRETIME = process.env.SERVER_TOKEN_EXPIRETIME || 3600;
 const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || 'coolIssuer';
 const SERVER_TOKEN_SECRET = process.env.SERVER_TOKEN_SECRET || 'superencryptedsecret';
@@ -14,7 +14,7 @@ export const config = {
 		url: MONGO_URL,
 	},
 	server: {
-		port: SERVER_PORT,
+		port: PORT,
 		token: {
 			expireTime: SERVER_TOKEN_EXPIRETIME,
 			issuer: SERVER_TOKEN_ISSUER,
