84ab279ec9a93dabbdd0adccb7378faf66128495
Valentinas <scenass@gmail.com>
2022-08-17 23:11:48 +0300

    FIX: CORS

--- server/server.ts
+++ server/server.ts
@@ -34,6 +34,7 @@ app.use(express.json());
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
 
 	if (req.method == 'OPTIONS') {
 		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
