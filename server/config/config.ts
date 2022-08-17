3c0973cafbe329c46c747a390dce132911244e57
Valentinas <scenass@gmail.com>
2022-08-17 22:57:00 +0300

    FIX: CORS

--- server/config/config.ts
+++ server/config/config.ts
@@ -7,7 +7,7 @@ const MONGO_URL = process.env.MONGO_URL ?? '';
 const PORT = process.env.PORT ?? 6000;
 const SERVER_TOKEN_EXPIRETIME = process.env.SERVER_TOKEN_EXPIRETIME ?? 3700;
 const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER ?? 'coolIssuer';
-const SERVER_TOKEN_SECRET = process.env.SERVER_TOKEN_SECRET ?? 'crazySecret';
+export const JWT_TOKEN = process.env.JWT_TOKEN ?? 'crazySecret2';
 
 export const config = {
 	mongo: {
@@ -18,7 +18,7 @@ export const config = {
 		token: {
 			expireTime: SERVER_TOKEN_EXPIRETIME,
 			issuer: SERVER_TOKEN_ISSUER,
-			secret: SERVER_TOKEN_SECRET,
+			secret: JWT_TOKEN,
 		},
 	},
 };
--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -62,7 +62,7 @@ const register = (req: Request, res: Response, next: NextFunction) => {
 const login = (req: Request, res: Response, next: NextFunction) => {
 	const { username, password } = req.body as IUser;
 
-	User.find({ username })
+	User.findOne({ username })
 		.exec()
 		.then((users) => {
 			if (users.length !== 1) {
