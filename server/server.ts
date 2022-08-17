150d16fc261bc6471ccc0d38a00dd267ff44104c
Valentinas <scenass@gmail.com>
2022-08-17 17:28:12 +0300

    FIX: CORS

--- server/server.ts
+++ server/server.ts
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(cors());
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'https://vinyl-auction.netlify.app/');
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
 	if (req.method == 'OPTIONS') {
