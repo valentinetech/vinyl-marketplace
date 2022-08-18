9efe6f8d01b656606339116cab834759c5b640a2
Valentinas <scenass@gmail.com>
2022-08-18 08:40:06 +0300

    FIX: Persistent token

--- client/src/features/Auth/utils/RequireAuth.tsx
+++ client/src/features/Auth/utils/RequireAuth.tsx
@@ -1,8 +1,9 @@
-import { useAppSelector } from 'app/store';
+// import { useAppSelector } from 'app/store';
 import { Outlet, useLocation, Navigate } from 'react-router-dom';
 
 const RequireAuth = () => {
-	const { userToken } = useAppSelector((state) => state.auth);
+	// const { userToken } = useAppSelector((state) => state.auth);
+	const userToken = localStorage.getItem('userToken');
 	const location = useLocation();
 
 	return userToken ? <Outlet /> : <Navigate to='/login' state={{ from: location }} replace />;
