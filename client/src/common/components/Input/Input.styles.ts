2643a7f4d211faf182de6393b1751ac4053934f5
Valentinas <scenass@gmail.com>
2022-08-22 20:13:26 +0300

    FIX: Form mobile view

--- client/src/common/components/Input/Input.styles.ts
+++ client/src/common/components/Input/Input.styles.ts
@@ -2,12 +2,12 @@ import { theme } from 'common/styles/theme';
 import styled from 'styled-components';
 
 const InputStyles = styled.input`
-  border: 1px solid white;
-  border-radius: 10px;
-  padding: 10px;
-  background-color: ${theme.colors.body};
-  color: ${theme.colors.white};
-  margin-bottom: 15px;
+	border: 1px solid white;
+	border-radius: 10px;
+	padding: 10px;
+	background-color: ${theme.colors.body};
+	color: ${theme.colors.white};
+	margin-bottom: 15px;
 `;
 
 export default InputStyles;
--- client/src/features/Auth/Register/Register.styles.ts
+++ client/src/features/Auth/Register/Register.styles.ts
@@ -4,37 +4,36 @@ import styled from 'styled-components';
 export { Section, Form, FormGroup, RegisterHeader, ButtonContainer };
 
 const Section = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 `;
 
 const Form = styled.form`
-  width: 100%;
-  max-width: 1120px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+	width: 100%;
+	max-width: 1120px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `;
 const FormGroup = styled.div`
-  font-family: inherit;
-  display: flex;
-  width: auto;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
-  border: 1px solid #e6e6e6;
-  border-radius: 5px;
-  margin: 50px 50px;
-  padding: 30px 50px;
+	font-family: inherit;
+	display: flex;
+	width: auto;
+	flex-direction: column;
+	align-items: center;
+	border: 1px solid #e6e6e6;
+	border-radius: 5px;
+	margin: 50px;
+	padding: 30px;
 `;
 
 const RegisterHeader = styled.h2`
-  margin-bottom: 20px;
-  color: ${theme.colors.brandSecondary};
+	margin-bottom: 20px;
+	color: ${theme.colors.brandSecondary};
 `;
 
 const ButtonContainer = styled.div`
-  margin-top: 20px;
+	margin-top: 20px;
 `;
