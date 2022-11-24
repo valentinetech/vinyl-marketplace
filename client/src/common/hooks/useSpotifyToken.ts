2cac5bf287c6d4f24251c7fc8265be11a3f6f7c0
Valentinas <scenass@gmail.com>
2022-11-24 23:51:27 +0200

    Netlify fix

--- client/src/common/hooks/useSpotifyToken.ts
+++ client/src/common/hooks/useSpotifyToken.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import qs from 'qs';
-import { PostToken } from '../../features/Home/models/Home.models';
+import { PostToken } from '../../features/Home/models/Home-models';
 const Buffer = require('buffer/').Buffer;
 
 export function useSpotifyToken() {
